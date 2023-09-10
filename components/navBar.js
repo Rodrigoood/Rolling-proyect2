15a26ccddd53e5ddf9c53c6a415fa2091c63d2e1
Franco Chaile <francochaile2013.14@gmail.com>
2023-09-10 16:41:37 -0300

    agrego navbar

--- components/navBar.js
+++ components/navBar.js
@@ -1,6 +1,34 @@
 const header = document.querySelector('header');
 
 const navBar = () => {
-const user = localStorage.getItem('userLog');
+  const user = localStorage.getItem('userLog') ||
+  undefined;
+  return (header.innerHTML = `
+  <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+  </li>
+  ${
+    user
+    ?  ''
+    : `
+  <li class="nav-item">
+    <a class="nav-link" href="./login.html">Login</a>
+  </li>
+  `}
+  ${
+    user 
+    ? ''
+    : `
+    <li class="nav-item">
+      <a class="nav-link" href="./regiser.html">Registro</a>
+    </li>
+  `}
+    <li class="nav-item">
+      <a class="nav-link" href="./contact.html">Contacto</a>
+    </li>
+</ul>`)
 
 }
+
+export default navBar;
